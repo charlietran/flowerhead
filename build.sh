57ebb5d142b74ba202e35d470616230d60321beb
Charlie Tran <hi@charlietran.com>
2018-10-09 19:56:59 -0400

    Add build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+../picotool/p8tool build flowerhead_build.p8 --lua-minify \
+ --lua flowerhead.p8   --gfx flowerhead.p8 --gff flowerhead.p8 \
+ --music flowerhead.p8 --map flowerhead.p8 --sfx flowerhead.p8
+comment='\
+-- f l o w e r h e a d \
+-- by charlie tran \
+-- this code is minified \
+-- the original source is here \
+-- github.com/charlietran/flowerhead '
+
+sed -i '' "s@__lua__@__lua__\
+$comment\
+@" flowerhead_build.p8
